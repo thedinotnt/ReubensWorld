44c002e3d36311d52949c6ebcb8583db6aecd73e
ReubenC <108216817+thedinotnt@users.noreply.github.com>
2024-08-23 13:41:05 +0100

    Create sw.js (serviceworker for shared array buffer for github page)

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js
+self.addEventListener("fetch", function (event) {
+  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        const newHeaders = new Headers(response.headers);
+        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+        const moddedResponse = new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: newHeaders,
+        });
+
+        return moddedResponse;
+      })
+      .catch(function (e) {
+        console.error(e);
+      })
+  );
+});
